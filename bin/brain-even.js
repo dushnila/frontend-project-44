0848d0cb14ac1d91864080e849f5c790159560d2
dushnila <psihochudovishe@gmail.com>
2023-02-26 01:28:47 +0300

    new structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,22 +1,7 @@
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+import askAndHello from '../src/cli.js';
+import isEven from '../src/even.js';
 
-export default (name) => {
-  let numberOfrounds = 3;
-  const task = 'Answer "yes" if the number is even, otherwise answer "no".';
-  console.log(task);
-  while (numberOfrounds > 0) {
-    const randomNumber = Math.floor(Math.random() * 100);
-    const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer: `);
-    if (answer === isEven(randomNumber)) {
-      console.log('Correct!');
-      numberOfrounds -= 1;
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${isEven(randomNumber)}".
-Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
+const name = askAndHello();
+isEven(name);
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
 import askAndHello from '../src/cli.js';
-import isEven from './brain-even.js';
 
-const name = askAndHello();
-isEven(name);
+askAndHello();
