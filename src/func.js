446d938cbce48e7f40959923970e2449e3b21b2f
dushnila <psihochudovishe@gmail.com>
2023-03-01 17:30:55 +0300

    try to fix

--- src/func.js
+++ src/func.js
@@ -3,14 +3,5 @@
 
 const randomiser = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const makeMathRow = (firstValue, ratio, rowLength) => {
-  const arr = [];
-  let accum = firstValue;
-  for (let i = 0; i < rowLength; i += 1) {
-    accum += ratio;
-    arr.push(accum);
-  }
-  return arr;
-};
-
+// как так то
 export default randomiser;
