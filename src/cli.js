4a134a19461e819983a2fe2714df5a753f7a654a
dushnila <psihochudovishe@gmail.com>
2023-03-05 10:20:13 +0300

    6) Может быть сделаем имя makeWelcome? 7)Имя возвращать не нужно. Мы его нигде снаружи не будет использовать.

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,4 @@ import readlineSync from 'readline-sync';
 export default () => {
   const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?');
   console.log(`Hello, ${name}!`);
-  return name;
 };
