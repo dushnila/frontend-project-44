b4d9d8f589f58e49e2bfe19b1c037712b738d80d
dushnila <psihochudovishe@gmail.com>
2023-03-06 16:45:44 +0300

    recode even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,10 @@
 import getRandomInRange from '../utils.js';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => (num % 2 === 0);
 
 const game = () => {
   const generateQuestion = getRandomInRange(0, 100);
-  const realAnswer = isEven(generateQuestion);
+  const realAnswer = isEven(generateQuestion) ? 'yes' : 'no';
   return [generateQuestion, realAnswer];
 };
 
