5fa20db8c5e0bfc2c46a48759b0da2299ab19843
dushnila <psihochudovishe@gmail.com>
2023-03-11 01:07:38 +0300

    change gcd game files

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import genericLogic from '../src/index.js';
 import gcdGame from '../src/games/gcd.js';
 
-const task = 'Find the greatest common divisor of given numbers.';
-genericLogic(task, gcdGame);
+gcdGame();
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,3 +1,4 @@
+import runEngine from '../index.js';
 import getRandomInRange from '../utils.js';
 
 const gcdFinder = (number1, number2) => {
@@ -14,12 +15,19 @@ const gcdFinder = (number1, number2) => {
   return gcd;
 };
 
-const game = () => {
-  const randomNumber1 = getRandomInRange(0, 101);
-  const randomNumber2 = getRandomInRange(0, 101);
+const generateRound = () => {
+  const randomNumber1 = getRandomInRange(0, 100);
+  const randomNumber2 = getRandomInRange(0, 100);
+  return [randomNumber1, randomNumber2];
+};
+
+const round = () => {
+  const [randomNumber1, randomNumber2] = generateRound();
   const generateQuestion = `${randomNumber1} ${randomNumber2}`;
   const realAnswer = String(gcdFinder(randomNumber1, randomNumber2));
   return [generateQuestion, realAnswer];
 };
 
-export default game;
+const rule = 'Find the greatest common divisor of given numbers.';
+
+export default () => runEngine(rule, round);
