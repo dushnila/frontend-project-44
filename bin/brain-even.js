4f8ba782e0d9dc214f58776cb72c5dfdfd1e1cef
dushnila <psihochudovishe@gmail.com>
2023-03-11 00:33:55 +0300

    change even game files

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import runEngine from '../src/index.js';
 import isEvenGame from '../src/games/even.js';
 
-const task = 'Answer "yes" if the number is even, otherwise answer "no".';
-runEngine(task, isEvenGame);
+isEvenGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,19 @@
+import runEngine from '../index.js';
 import getRandomInRange from '../utils.js';
 
 const isEven = (num) => (num % 2 === 0);
 
-const game = () => {
-  const generateQuestion = getRandomInRange(0, 100);
+const generateRound = () => {
+  const num = getRandomInRange(0, 100);
+  return num;
+};
+
+const round = () => {
+  const generateQuestion = generateRound();
   const realAnswer = isEven(generateQuestion) ? 'yes' : 'no';
   return [generateQuestion, realAnswer];
 };
 
-export default game;
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+export default () => runEngine(rule, round);
