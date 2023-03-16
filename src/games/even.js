f87b4ee242c4bc3ab1e96bbede86829c4a934145
dushnila <psihochudovishe@gmail.com>
2023-03-16 22:53:33 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -4,16 +4,11 @@ import getRandomInRange from '../utils.js';
 const isEven = (num) => (num % 2 === 0);
 
 const generateRound = () => {
-  const num = getRandomInRange(0, 100);
-  return num;
-};
-
-const round = () => {
-  const generateQuestion = generateRound();
+  const generateQuestion = getRandomInRange(0, 100);
   const realAnswer = isEven(generateQuestion) ? 'yes' : 'no';
   return [generateQuestion, realAnswer];
 };
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-export default () => runEngine(rule, round);
+export default () => runEngine(rule, generateRound);
